c274bced3471cfdbcf3075940bd8c8076fc72733
Goce Joncheski <jonceski@its-sk.com.mk>
2018-07-27 14:27:28 +0200

    remove

--- test/conf/sdp-ctrl-client/drop_service_access.sql
+++
@@ -1,8 +0,0 @@
---
--- Database: `sdp_test`
---
-USE `sdp_test`;
-
-DELETE FROM `sdp_test`.`sdpid_service` 
-WHERE `sdpid_service`.`sdpid` = 333 AND
-      `sdpid_service`.`service_id` = 1;
--- test/conf/sdp-ctrl-client/sdp_test_create.sql
+++
@@ -1,9 +0,0 @@
---
--- Database: `sdp_test`
---
-DROP DATABASE IF EXISTS `sdp_test`;
-CREATE DATABASE IF NOT EXISTS `sdp_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
-
--- Privileges for `sdp_test`@`localhost`
-GRANT ALL PRIVILEGES ON `sdp\_test`.* TO 'sdp_test'@'localhost';
-
